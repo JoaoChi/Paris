528add46796e3df1a2f1dd486148c3d3e77883c1
Joao Gabriel Chiarello <joao.chiarello@angellira.com.br>
2024-07-29 16:51:49 -0300

    Login funcionando com acesso ao banco, não permitindo criar conta com o email já existente.

--- app/src/main/java/com/angellira/paris/CadastroActivity.kt
+++ app/src/main/java/com/angellira/paris/CadastroActivity.kt
@@ -2,11 +2,13 @@ package com.angellira.paris
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.angellira.paris.databinding.ActivityCadastroBinding
 import com.angellira.paris.network.ParisApi
 import com.angellira.paris.network.User
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.launch
 
 class CadastroActivity : AppCompatActivity() {
@@ -20,22 +22,61 @@ class CadastroActivity : AppCompatActivity() {
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.buttonCadastro.setOnClickListener {
             val name = binding.textNome.text.toString()
             val email = binding.textEmail.text.toString()
             val password = binding.textSenha.text.toString()
             val img = binding.botaoAddImagem.text.toString()
+            val context = this
 
             val user = User(name, email, password, img)
             lifecycleScope.launch {
-                parisApi.saveUser(user)
+                val isValid = checkEmail(email)
+                if (isValid){
+                    MaterialAlertDialogBuilder(context)
+                        .setTitle(resources.getString(R.string.title))
+                        .setMessage(resources.getString(R.string.supporting_text))
+
+                        .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+                        }
+                        .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+                        }
+                        .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+                        }
+                        .show()
+                }
+                else{
+                    parisApi.saveUser(user)
+                    val loginActivity = Intent(context, LoginActivity::class.java)
+                    startActivity(loginActivity)
+                }
             }
-
-            val loginActivity = Intent(this, LoginActivity::class.java)
-            startActivity(loginActivity)
-
-
         }
+    }
+    private suspend fun checkEmail(email: String): Boolean {
+        return if (email.isNotEmpty()) {
+            val users = parisApi.getUsers()
+            users.values.any { it.email == email }
+        } else {
+            false
+        }
+    }
+    fun suggestNewEmail(existingEmails: List<String>, email: String): String {
+        var suggestedEmail = email
+        var counter = 1
+
+        while (existingEmails.contains(suggestedEmail)) {
+            val emailParts = email.split("@")
+            val localPart = emailParts[0]
+            val domainPart = emailParts[1]
+
+            suggestedEmail = "$localPart$counter@$domainPart"
+            counter++
+        }
+
+        return suggestedEmail
     }
 }
 
--- app/src/main/java/com/angellira/paris/LoginActivity.kt
+++ app/src/main/java/com/angellira/paris/LoginActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.angellira.paris.databinding.ActivityLoginBinding
 import com.angellira.paris.network.ParisApi
+import com.angellira.paris.network.ParisApiService
 import com.angellira.paris.network.User
 import com.angellira.paris.preferences.PreferencesManager
 import kotlinx.coroutines.launch
@@ -33,45 +34,48 @@ class LoginActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
+            val context = this
 
-//            val users = ParisApi.retrofitService.getUsers()
-//            val usersExist = users.values.any { it.email == email && it.password == password }
+            lifecycleScope.launch {
+                val isValid = checkCredentials(email, password)
+                if (isValid) {
+                    val startMain = Intent(context, MainActivity::class.java)
+                    startActivity(startMain)
+                    finish()
+                } else {
+                    Toast.makeText(context, "Email ou senha inválidos.", Toast.LENGTH_SHORT).show()
+                }
+            }
 
-            if (checkCredentials(email, password)) {
-                preferencesManager.isLogged = true
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+        }
+    }
+
+        private fun setupRegisterButton() {
+            binding.registerTextView.setOnClickListener {
+                startActivity(Intent(this, CadastroActivity::class.java))
+            }
+        }
+
+        private fun setupView() {
+            enableEdgeToEdge()
+            binding = ActivityLoginBinding.inflate(layoutInflater)
+
+            setContentView(binding.root)
+            ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(64, systemBars.top, 64, systemBars.bottom)
+                insets
+            }
+        }
+
+        private suspend fun checkCredentials(email: String, password: String): Boolean {
+            return if (email.isNotEmpty() && password.isNotEmpty()) {
+                val users = parisApi.getUsers()
+                users.values.any { it.email == email && it.password == password }
             } else {
-                Toast.makeText(this, "Email ou senha inválidos.", Toast.LENGTH_SHORT).show()
+                false
             }
         }
     }
 
-    private fun setupRegisterButton() {
-        binding.registerTextView.setOnClickListener {
-            startActivity(Intent(this, CadastroActivity::class.java))
-        }
-    }
 
-    private fun setupView() {
-        enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(64, systemBars.top, 64, systemBars.bottom)
-            insets
-        }
-    }
-
-    private fun checkCredentials(email: String, password: String): Boolean {
-        val isValid = email.isNotEmpty() && password.isNotEmpty()
-
-
-        // buscar no webapi se o email e senha são válidos e retornar true ou false
-
-        return isValid
-    }
-}
