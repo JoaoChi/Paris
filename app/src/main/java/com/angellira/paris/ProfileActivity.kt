cb028836f6d75ae20270ae0fa49debe80e3a9c1e
Joao Gabriel Chiarello <joao.chiarello@angellira.com.br>
2024-07-31 14:04:56 -0300

    adicionada toolbar na tela de perfil

--- app/src/main/java/com/angellira/paris/ProfileActivity.kt
+++ app/src/main/java/com/angellira/paris/ProfileActivity.kt
@@ -1,20 +1,39 @@
 package com.angellira.paris
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.angellira.paris.databinding.ActivityMainBinding
+import com.angellira.paris.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupView()
+        setSupportActionBar(findViewById(R.id.barra_tarefasProfile))
+
+        }
+
+    private fun setupView() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.itens, menu)
+        return true
     }
 }
